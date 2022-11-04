da12c08c8f646a23cb66bc32db034ae774801975
IncompleteDesire <117275260+IncompleteDesire@users.noreply.github.com>
2022-11-04 18:52:14 +0700

    Update retro.lua

--- retro.lua
+++ retro.lua
@@ -29,7 +29,7 @@ local plantButton = autofarmTab.new("Switch", {text=" plant 5 boxes "})
 plantButton.event:Connect(function()
     for i = 1, 1 do
         local ohString1 = "Plant Box"
-        local ohCFrame2 = CFrame.new(Players.LocalPlayer.Character.HumanoidRootPart.Position.X,Players.LocalPlayer.Character.HumanoidRootPart.Position.Y-3,Players.LocalPlayer.Character.HumanoidRootPart.Position.Z+i, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+        local ohCFrame2 = CFrame.new(Players.LocalPlayer.Character.HumanoidRootPart.Position.X,Players.LocalPlayer.Character.HumanoidRootPart.Position.Y-3,Players.LocalPlayer.Character.HumanoidRootPart.Position.Z+i, 0, 0, 0, 0, 1, 0, 0, 0, 0)
         local ohNumber3 = 0
         game:GetService("ReplicatedStorage").Events.PlaceStructure:FireServer(ohString1, ohCFrame2, ohNumber3)
     end
